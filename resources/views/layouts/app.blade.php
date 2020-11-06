5beb949ee5a2d63f9f3045512224db69e9a97336
Luis Frisneda <frisneda92@gmail.com>
2020-11-06 18:07:58 -0430

    Middleware, group de rutas afectadas y redireccion de loggin exitoso

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,9 +37,13 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    @auth
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/usuarios') }}">Usuarios</a>
+                        </li>
                     </ul>
+                    @endauth
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('/cursos', 'CourseController');
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('/cursos', 'CourseController');
 
-Route::get('/unidades/create/{curso}', 'LessonController@create');
-Route::post('/unidades', 'LessonController@store');
-Route::get('/unidades/{curso}', 'LessonController@show');
-Route::get('/unidades/{unidad}/edit', 'LessonController@edit');
-Route::put('/unidades/{unidad}', 'LessonController@update');
-Route::delete('/unidades/{unidad}', 'LessonController@destroy');
+    Route::resource('/usuarios', 'UserController');
 
-Route::get('/soportes/{unidad}', 'SupportController@index');
-Route::get('/soportes/create/{unidad}', 'SupportController@create');
-Route::post('/soportes/{unidad}', 'SupportController@store');
-Route::get('/soportes/{material}/edit', 'SupportController@edit');
-Route::put('/soportes/{material}', 'SupportController@update');
-Route::delete('/soportes/{material}', 'SupportController@destroy');
+    Route::get('/unidades/create/{curso}', 'LessonController@create');
+    Route::post('/unidades', 'LessonController@store');
+    Route::get('/unidades/{curso}', 'LessonController@show');
+    Route::get('/unidades/{unidad}/edit', 'LessonController@edit');
+    Route::put('/unidades/{unidad}', 'LessonController@update');
+    Route::delete('/unidades/{unidad}', 'LessonController@destroy');
+
+    Route::get('/soportes/{unidad}', 'SupportController@index');
+    Route::get('/soportes/create/{unidad}', 'SupportController@create');
+    Route::post('/soportes/{unidad}', 'SupportController@store');
+    Route::get('/soportes/{material}/edit', 'SupportController@edit');
+    Route::put('/soportes/{material}', 'SupportController@update');
+    Route::delete('/soportes/{material}', 'SupportController@destroy');
+});
